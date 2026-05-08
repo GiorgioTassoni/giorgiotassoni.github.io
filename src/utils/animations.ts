f7cd77529e5d308601e2cb73f52d76dac9488478
GioViale <giorgiotassoni@protonmail.com>
2026-05-08 19:10:56 +0200

    First commit: portfolio ready

---
+++ src/utils/animations.ts
@@ -0,0 +1,14 @@
+export const mySlideUp = {
+  old: {
+    name: 'slide-down-fade-out',
+    duration: '0.3s',
+    easing: 'ease-out',
+    fillMode: 'forwards',
+  },
+  new: {
+    name: 'slide-up-fade-in',
+    duration: '0.5s',
+    easing: 'ease-out',
+    fillMode: 'backwards',
+  }
+};
