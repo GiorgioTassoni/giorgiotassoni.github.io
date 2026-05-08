0c33d17142aece7acc510a3326cd5d459bacd046
GioViale <giorgiotassoni@protonmail.com>
2026-05-08 19:28:58 +0200

    Added Deploy.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://giorgiotassoni.github.io',
+    base: 'giorgiotassoni',
     vite: {
         plugins: [tailwindcss()],
     },
