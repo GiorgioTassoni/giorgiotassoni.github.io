54fb41be78476d1c4cce456acc06e2e6cd46e5ce
GioViale <giorgiotassoni@protonmail.com>
2026-05-13 20:08:10 +0200

    Changed contact method to (mailto:)

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://giorgiotassoni.github.io',
-    base: 'giorgiotassoni',
+    base: '',
     vite: {
         plugins: [tailwindcss()],
     },
